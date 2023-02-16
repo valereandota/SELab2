896507fab239698b47d6c7b61e91080a9c9654af
tom <welter.tom@hotmail.com>
2023-02-16 15:29:17 +0100

    no message

--- app/src/main/java/com/example/selab2/MainActivity.kt
+++ app/src/main/java/com/example/selab2/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Switch
+import android.widget.TextView
 import com.example.selab2.databinding.ActivityMainBinding
+import com.google.android.material.slider.Slider
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -22,9 +25,24 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var doorSwitch: Switch
+    private lateinit var lightSwitch: Switch
+    private lateinit var windowSwitch: Switch
+    private lateinit var lightText:TextView
+    private lateinit var windowText:TextView
+    private lateinit var doorText:TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val database = Firebase.database("https://selab2-fd87c-default-rtdb.europe-west1.firebasedatabase.app")
+
+        doorSwitch = findViewById(R.id.doorSwitch)
+        doorText = findViewById(R.id.doorView)
+        lightSwitch = findViewById(R.id.lightsSwitch)
+        lightText = findViewById(R.id.lightsView)
+        windowSwitch = findViewById(R.id.windowSwitch)
+        windowText = findViewById(R.id.windowView)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -34,6 +52,20 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                val post = dataSnapshot.getValue<Post>()
+                // ...
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        postReference.addValueEventListener(postListener)
 
     }
 
