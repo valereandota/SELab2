b29b493f84490bfd19d47ebf92dd2f0e83aa58eb
Tom Welter <welter.tom@hotmail.com>
2023-05-08 01:19:27 +0200

    finished

--- app/src/main/java/com/example/selab2/MainActivity.kt
+++ app/src/main/java/com/example/selab2/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.example.selab2
 
+import android.Manifest
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Button
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Switch
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.selab2.databinding.ActivityMainBinding
-import com.google.android.material.slider.Slider
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
@@ -31,77 +38,150 @@ class MainActivity : AppCompatActivity() {
     private lateinit var lightText:TextView
     private lateinit var windowText:TextView
     private lateinit var doorText:TextView
+    private lateinit var textListenerButton: Button
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 1
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val database = Firebase.database("https://selab2-fd87c-default-rtdb.europe-west1.firebasedatabase.app")
 
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         doorSwitch = findViewById(R.id.doorSwitch)
         doorText = findViewById(R.id.doorView)
         lightSwitch = findViewById(R.id.lightsSwitch)
         lightText = findViewById(R.id.lightsView)
         windowSwitch = findViewById(R.id.windowSwitch)
         windowText = findViewById(R.id.windowView)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        textListenerButton = findViewById(R.id.textListener)
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        val doorReference = database.getReference("door")
+        val lightReference = database.getReference("light")
+        val windowReference = database.getReference("window")
 
-        val postListener = object : ValueEventListener {
+        val doorListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 // Get Post object and use the values to update the UI
-                val post = dataSnapshot.getValue<Post>()
-                // ...
+                val door = dataSnapshot.value
+                doorSwitch.isChecked = door == "open"
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
                 // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                Log.w("loadDoor:onCancelled", databaseError.toException())
             }
         }
-        postReference.addValueEventListener(postListener)
+        val lightListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                val light = dataSnapshot.value
+                lightSwitch.isChecked = light == "on"
 
-    }
-
- fun writeDB(value:String, db: String){
-     val database = Firebase.database("https://selab2-fd87c-default-rtdb.europe-west1.firebasedatabase.app")
-     val myRef = database.getReference(db)
-     myRef.setValue(value)
- }
-    fun readDB(db : String){
-        val database = Firebase.database("https://selab2-fd87c-default-rtdb.europe-west1.firebasedatabase.app")
-        val myRef = database.getReference(db)
-        myRef.addValueEventListener(object: ValueEventListener {
-
-            override fun onDataChange(snapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = snapshot.getValue<String>()
-                Log.d("TEST", "Value is: " + value)
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("TEST", "Failed to read value.", error.toException())
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("loadLight:onCancelled", databaseError.toException())
+            }
+        }
+        val windowListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Get Post object and use the values to update the UI
+                val window = dataSnapshot.value
+                windowSwitch.isChecked = window == "open"
             }
 
-        })
-    }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("loadWindow:onCancelled", databaseError.toException())
+            }
+        }
+        doorReference.addValueEventListener(doorListener)
+        lightReference.addValueEventListener(lightListener)
+        windowReference.addValueEventListener(windowListener)
+        doorSwitch.setOnClickListener{
+            if(doorSwitch.isChecked) {
+                doorReference.setValue("closed")
+            } else if(!doorSwitch.isChecked) {
+                doorReference.setValue("open")
+            }
+        }
+        lightSwitch.setOnClickListener{
+            if(lightSwitch.isChecked) {
+                lightReference.setValue("off")
+            } else if(!lightSwitch.isChecked) {
+                lightReference.setValue("on")
+            }
+        }
+        windowSwitch.setOnClickListener{
+            if(windowSwitch.isChecked) {
+                windowReference.setValue("closed")
+            } else if(!windowSwitch.isChecked) {
+                windowReference.setValue("open")
+            }
+        }
+        val speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+        val recognitionListener = object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle?) {}
+            override fun onBeginningOfSpeech() {}
+            override fun onRmsChanged(rmsdB: Float) {}
+            override fun onBufferReceived(buffer: ByteArray?) {}
+            override fun onEndOfSpeech() {}
+            override fun onError(error: Int) {}
+
+            override fun onResults(results: Bundle) {
+                val matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                Log.d("THISISTHERESULT", matches.toString())
+                if (matches != null && matches.contains("light on")) {
+                    lightReference.setValue("on")
+                }else if (matches != null && matches.contains("open the door")) {
+                    doorReference.setValue("open")
+                }else if (matches != null && matches.contains("open window")) {
+                    windowReference.setValue("open")
+                }else if (matches != null && matches.contains("close window")) {
+                    windowReference.setValue("closed")
+                }else if (matches != null && matches.contains("close door")) {
+                    doorReference.setValue("closed")
+                }else if (matches != null && matches.contains("light off")) {
+                    lightReference.setValue("off")
+                }
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {}
+            override fun onEvent(eventType: Int, params: Bundle?) {}
+        }
+        speechRecognizer.setRecognitionListener(recognitionListener)
+        textListenerButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), PERMISSION_REQUEST_CODE)
+            } else {
+                val recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                    putExtra(
+                        RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                    )
+                    putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this@MainActivity.packageName)
+                    putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3)
+                }
+                speechRecognizer.startListening(recognizerIntent)
+            }}
+        }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
